3090903c02521a02a9a8146e653127a0cec58707
farzad foruozanfar <foruozanfar2000f@gmail.com>
2023-05-20 19:28:18 +0330

    create lang data

---
+++ lang/lang-en.php
@@ -0,0 +1,13 @@
+<?php
+return $lang = [
+    'FileNotFound' => "Input File Not Found",
+    'InvalidFileNameSize' => "The number of characters in the input file is greater than %d",
+    'InvalidCharInFileName' => "The file name contains illegal characters",
+    'FileSizeExceeded' => "Input file size is greater than %d kb",
+    'InvalidFileExtension' => "Input file extension is not allowed, only # files are accepted",
+    'InvalidFileType' => "Input file type is not allowed",
+    'InvalidContents' => "Input file content is not allowed",
+    'AntivirusFileNotFound' => "Antivirus executable file not found",
+    'InfectedFile' => "Input file is illegal and contains virus",
+    'ClamAVError' => "Antivirus error: "
+];
---
+++ lang/lang-fa.php
@@ -0,0 +1,13 @@
+<?php
+return $lang = [
+    'FileNotFound' => 'فایل ورودی یافت نشد',
+    'InvalidFileNameSize' => 'تعداد کاراکتر های فایل ورودی از %d بیشتر است',
+    'InvalidCharInFileName' => 'نام فایل حاوی کاراکتر های غیرمجاز می باشد',
+    'FileSizeExceeded' => 'حجم فایل ورودی بیشتر از %d کیلوبایت است',
+    'InvalidFileExtension' => 'پسوند فایل ورودی غیر مجاز می باشد فقط فایل های "#" مورد قبول هستند',
+    'InvalidFileType' => 'نوع فایل ورودی غیر مجاز می باشد',
+    'InvalidContents' => 'محتوای فایل ورودی غیر مجاز می باشد',
+    'AntivirusFileNotFound' => 'فایل اجرایی آنتی ویروس یافت نشد',
+    'InfectedFile' => 'فایل ورودی غیرمجاز و حاوی ویروس است',
+    'ClamAVError' => 'خطای آنتی ویروس :'
+];
