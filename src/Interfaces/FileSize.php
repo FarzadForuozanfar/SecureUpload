85487ce6f08ff8e7e8614bc63fa667c3da9b9f24
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2025-04-03 18:41:28 +0330

    add FileSize interface for defining file size constants

---
+++ src/Interfaces/FileSize.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace SecureUpload\Interfaces;
+
+interface FileSize
+{
+    public const THREE_MG = 3072;
+    public const FIVE_MG  = 5120;
+    public const SEVEN_MG = 7168;
+    public const TEN_MG   = 10240;
+}
